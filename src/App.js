54507645249a837fcac946bf659f0dea5a5f0efe
unknown <vishalofl.mufc@gmail.com>
2023-11-21 19:29:57 +0530

    fr

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold">
+      <h3>This is netflix App 1</h3>
+      <h2>test dara 12</h2>
+    </div>
+  );
+}
+
+export default App;
